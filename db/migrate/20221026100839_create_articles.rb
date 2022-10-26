990d8f5b1ee471653011c452aad56cc7f0b88ba6
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-10-26 15:47:15 +0300

    Show a particular article

---
+++ db/migrate/20221026100839_create_articles.rb
@@ -0,0 +1,11 @@
+# rails generate migration create_articles
+
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+      t.timestamps
+    end 
+  end
+end
