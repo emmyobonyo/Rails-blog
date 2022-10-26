5a0059bd2e1c515424315432070514d844d104bf
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-10-26 16:49:07 +0300

    Allow a user to post to the database

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   #This is the root class. The page that will open on application load
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  # rails --routes expanded : to see all the routes
+  resources :articles, only: [:show, :index, :new, :create]
 end
